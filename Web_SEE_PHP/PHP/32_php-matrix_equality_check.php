372da2f877f15e1fbe6d272c82ff236b63b45a09
sachindsilva16 <sachindsilvad@gmail.com>
2023-05-21 20:36:24 +0530

    Initial Commit

--- Web_SEE_PHP/PHP/32_php-matrix_equality_check.php
+++ Web_SEE_PHP/PHP/32_php-matrix_equality_check.php
@@ -1,63 +1,36 @@
 <h1>CHECK WHETHER MATRIX ELEMENTS ARE SAME</h1>
 <?php
 
-$a = array(
-    array(1, 2, 3),
-    array(3, 4, 5),
-    array(5, 6, 7)
+$a=array(
+    array(1,1,1,1),
+    array(2,2,2,2),
+    array(3,3,3,3),
+    array(4,4,4,4)
 );
 
-$b = array(
-    array(1, 2, 3),
-    array(3, 4, 5),
-    array(5, 6, 7)
+$b=array(
+    array(1,1,1,1),
+    array(2,2,2,2),
+    array(3,3,3,3),
+    array(4,4,4,4)
 );
 
 
-echo "Matrix elements are <br>";
-echo "Matrix 1 <br>";
-$row1 = count($a);
-$col1 = count($a[0]);
+function areSame($A,$B){
+    for($i=0;$i<4;$i++)
+        for($j=0;$j<4;$j++)
+            if($A[$i][$j]!=$B[$i][$j])
+                return 0;
+    return 1;
 
-for ($i = 0; $i < $row1; $i++) {
-    for ($j = 0; $j < $col1; $j++) {
-        echo $a[$i][$j] . " ";
-    }
-    echo "<br>";
 }
-echo "<hr>";
 
-echo "Matrix 2 <br>";
-$row2 = count($b);
-$col2 = count($b[0]);
-
-for ($i = 0; $i < $row2; $i++) {
-    for ($j = 0; $j < $col2; $j++) {
-        echo $b[$i][$j] . " ";
-    }
-    echo "<br>";
-}
-echo "<hr>";
-
-
-if ($row1 != $row2 || $col1 != $col2) {
+if(areSame($a,$b)==1){
+    echo "Matrices are equal";
+} else{
     echo "Matrices are not equal";
-} else {
-    for ($i = 0; $i < $row1; $i++) {
-        for ($j = 0; $j < $col1; $j++) {
-            if ($a[$i][$j] != $b[$i][$j]) {
-                $flag = false;
-                break;
-            }
-        }
-    }
-
-    if ($flag = true) {
-        print("Matrices are equal..");
-    } else {
-        print("Matrices are not equal..");
-    }
 }
+
 
 
 ?>
